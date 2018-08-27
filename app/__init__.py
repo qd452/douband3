09e33d71b5afcf745c6ab1df740e7c78decb3e34
Qu Dong <qudong452@gmail.com>
2018-08-27 23:50:28 +0800

    Setup the celery environment

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from app.config import Config
 from flask_marshmallow import Marshmallow
+from celery import Celery
 
 db = SQLAlchemy()
 ma = Marshmallow()
 migrate = Migrate()
-
+celery = Celery(__name__, broker=Config.CELERY_BROKER_URL)
 
 def create_app(config_class=Config):
     app = Flask(__name__, static_url_path='') #static_url_path cause static
@@ -17,6 +18,8 @@ def create_app(config_class=Config):
     db.init_app(app)
     ma.init_app(app)
     migrate.init_app(app, db)
+
+    celery.conf.update(app.config)
 
     from .dbanalyst import dbanalyst
     app.register_blueprint(dbanalyst)
--- web_runner.py
+++ web_runner.py
@@ -1,5 +1,7 @@
-from app import create_app
+from app import create_app, celery
 
 app = create_app()
+app.app_context().push()
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
